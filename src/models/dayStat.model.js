77d821ae36f3c354f120c0021665cdf644740f09
Teo Jin Cheng <jinchengproject@gmail.com>
2020-02-21 13:41:10 +0800

    update stat schema to remove unique property

--- src/models/dayStat.model.js
+++ src/models/dayStat.model.js
@@ -3,8 +3,7 @@ const mongoose = require("mongoose");
 const statsSchema = new mongoose.Schema({
   dayNumber: {
     type: Number,
-    required: true,
-    unique: true
+    required: true
   },
   costPerCup: {
     type: Number,
