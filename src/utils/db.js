0bde3206a660881a579f695e3f08a9ef1d0548dd
teojincheng <jinchengproject@gmail.com>
2020-07-12 14:29:52 +0800

    change env variable

--- src/utils/db.js
+++ src/utils/db.js
@@ -3,11 +3,12 @@ const mongoose = require("mongoose");
 const mongoOptions = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true
+  useCreateIndex: true,
 };
 
 const dbName = "lemonadeTycoon";
-const dbUrl = process.env.MONGODB_URI || "mongodb://localhost:27017/" + dbName;
+const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/" + dbName;
+//const dbUrl = process.env.MONGODB_URI || "mongodb://localhost:27017/" + dbName;
 //const dbUrl = global.__MONGO_URI__ || "mongodb://localhost:27017/" + dbName;
 mongoose.connect(dbUrl, mongoOptions);
 const db = mongoose.connection;
